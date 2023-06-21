7d039b771e874a5e68382b9300022459956b1dc6
Edwin Morara <gudwinoigo@gmail.com>
2023-06-20 19:02:25 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+int main(void)
+{
+	print _times_table(3);
+	_putchar('\n');
+	print _times_table(5);
+        _putchar('\n');
+	print _times_table(98);
+        _putchar('\n');
+	print _times_table(12);
+        return (0);
